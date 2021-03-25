06274730d7902ecf09fd434e396cd093a48192ac
esplab <peabryan95@gmail.com>
2021-03-25 09:57:35 -0400

    Added checks for LCD_ENABLED

--- main/main.cpp
+++ main/main.cpp
@@ -42,13 +42,17 @@ void app_main()
 	int ret;
 	for(int i = 0; i < 127; i++)
 	{
-		lcd.clear();
+		if(LCD_ENABLED)
+		{
+			lcd.clear();
+			lcd.setCursor(0, 0);
+			lcd.print("Trying addr.");
+			lcd.setCursor(0, 1);
+			lcd.print(i);
+		}
 		cfg.atcai2c.slave_address = i;
-		lcd.setCursor(0, 0);
-		lcd.print("Trying addr.");
-		lcd.setCursor(0, 1);
-		lcd.print(i);
 		ret = atcab_init(&cfg);
+		printf("Checking address %02X\n", i);
 		if(ret == 0)
 		{
 			printf("ECC608 initialized at address %02X\n", i);
@@ -67,13 +71,16 @@ void app_main()
 		if(i % 8 == 0) printf("\n");
 	}
 
-	// Display first 32 bytes of the key on the ECC608
-	for(int i = 0; i < 16; i++)
+	// Display first 32 bytes of the key on the DHT22
+	if(LCD_ENABLED)
 	{
-		for(int j = 0; j < 2; j++)
+		for(int i = 0; i < 16; i++)
 		{
-			lcd.setCursor(i, j);
-			lcd.print(temppubkey[i * (j + 1)]);
+			for(int j = 0; j < 2; j++)
+			{
+				lcd.setCursor(i, j);
+				lcd.print(temppubkey[i * (j + 1)]);
+			}
 		}
 	}
 
@@ -85,7 +92,7 @@ void app_main()
 
 	if(!(DHT22_ENABLED && LCD_ENABLED)) return;
 
-	while(1)
+	while(LCD_ENABLED && DHT22_ENABLED)
 	{
 		int ret = readDHT();
 		errorHandler(ret);
